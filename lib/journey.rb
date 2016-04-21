4c8950041a82e057336d10b148f66a8e6a8d65bc
Matthew Ward <matthewward@Matthews-MacBook-Pro-5.local>
2016-04-21 12:08:09 +0100

    Added maximum fare and defined #complete?. Refactored

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,14 +1,25 @@
 class Journey
-attr_reader :entry_station, :exit_station
+
+  attr_reader :entry_station, :exit_station
+
+  MINIMUM_FARE = 1
+  MAXIMUM_FARE = 6
+
   def initialize(station)
     @entry_station = station
-    @complete = false
   end
+
   def finish(station)
-    @complete = true
+    @exit_station = station
+  end
+
+  def fare
+    return MAXIMUM_FARE unless complete?
+    MINIMUM_FARE
   end
 
   def complete?
-    @complete
+    @entry_station && @exit_station != nil
   end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,18 +1,40 @@
 require 'journey'
 
 describe Journey do
-let(:journey) {described_class.new(entry_station)}
-let(:entry_station) {double :entry_station }
-let(:exit_station) {double :exit_station }
+  let(:journey) {described_class.new(entry_station)}
+  let(:entry_station) {double :entry_station }
+  let(:exit_station) {double :exit_station }
+  let(:min_fare) { described_class::MINIMUM_FARE }
+  let(:max_fare) { described_class::MAXIMUM_FARE }
+
   it "has an entry station" do
     expect(journey.entry_station).to eq entry_station
   end
-  it "journey is not completed at the start" do
-    expect(journey).to_not be_complete
 
-  end
-  it "journey is completed at the end" do
+  it "has an exit station" do
     journey.finish(exit_station)
-    expect(journey).to be_complete
-end
+    expect(journey.exit_station).to eq exit_station
+  end
+
+  context 'when finishing journey' do
+    context 'on an incomplete journey' do
+      it 'charges a maximum fare' do
+        expect(journey.fare).to be max_fare
+      end
+    end
+
+    context 'on a complete journey' do
+      before { journey.finish(exit_station) }
+        it "it is set to complete" do
+          expect(journey).to be_complete
+        end
+
+        it "it charges a minimum fare" do
+          expect(journey.fare).to be min_fare
+        end
+    end
+  end
+
+
+
 end
